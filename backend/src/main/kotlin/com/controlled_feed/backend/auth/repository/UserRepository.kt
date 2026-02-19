feb771508e85114417dfd9bb81864cb15a45cd52
Hari-chandan-28 <nakka.hari01@gmail.com>
2026-02-19 16:13:16 +0530

    UserRepository Updated

--- backend/src/main/kotlin/com/controlled_feed/backend/auth/repository/UserRepository.kt
+++ backend/src/main/kotlin/com/controlled_feed/backend/auth/repository/UserRepository.kt
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository
 import java.util.Optional
 
 interface UserRepository : JpaRepository<User, Long>{
-    fun findByUsername(username: String): Optional<User>
+    fun findByEmail(email: String): Optional<User>
     fun existsByEmail(email: String): Boolean
 }
